25686631757e184fec1bc68d4ecddc6c3fb4d755
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2019-12-17 00:52:15 +0800

    fixing _ inventory

--- Assets/Scripts/Managers/Interface/Inventory.cs
+++ Assets/Scripts/Managers/Interface/Inventory.cs
@@ -17,25 +17,32 @@ public class Inventory : MonoBehaviour, Manager
     public string open_sound;
     public string beep_sound;
 
+    private List<Item> inventoryItemList;
+    private List<Item> inventoryTabList; //활성화시 분류
+
+    [SerializeField]
+    private Text name_text;
+
+    //탭
+    [SerializeField]
+    private Transform tf_tab;
     private InventorySlot[] slots; //item slots
 
-    private List<Item> inventoryItemList;
-    private List<Item> inventoryTabList;
+    //상세
+    [SerializeField]
+    private Transform tf_item;
+    private Text description_Text;
 
-    public Text description_Text;
-    public string[] tabDescription;
 
-    public Transform tf;
-
-    public GameObject go; //인벤토리 창 활성화
-    public GameObject[] selectedTabImages;
-    public GameObject goOOC; //선택지 활성화
-    public GameObject prefab_FloatingText;
+    private GameObject go; //인벤토리 창 활성화
+    private GameObject goOOC; //선택지 활성화
+    //public GameObject prefab_FloatingText;
 
     private int selectedItem;
-    private int selectedTab;
+    private int selectedTab; //숫자로 사람과 사물을 구분. 
 
     private int page; //페이지
+    private int page2;
     private int slotCount; //활성화된 슬롯 수
     private const int MAX_SLOTS_COUNT = 8; //최대 슬롯 수
     
@@ -61,7 +68,7 @@ public class Inventory : MonoBehaviour, Manager
         
         inventoryItemList = new List<Item>();
         inventoryTabList = new List<Item>();
-        slots = tf.GetComponentsInChildren<InventorySlot>();
+        slots = tf_tab.GetComponentsInChildren<InventorySlot>(); //tab. 4X2
     }
     public List<Item> SaveItem()
     {
@@ -71,7 +78,7 @@ public class Inventory : MonoBehaviour, Manager
     {
         inventoryItemList = _item;
     }
-    public void ReturnItem(Item _item)
+    public void ReturnItem(Item _item) //?
     {
         inventoryItemList.Add(_item);
     }
@@ -98,52 +105,15 @@ public class Inventory : MonoBehaviour, Manager
 
     public void ShowTab()
     {
-        RemoveSlot();
-        SelectedTab();
-    }//탭 활성화
-    public void SelectedTab()
-    {
-        StopAllCoroutines();
-        Color color = selectedTabImages[selectedTab].GetComponent<Image>().color;
-        color.a = 0f;
-        for(int i = 0; i < selectedTabImages.Length; i++)
-            selectedTabImages[i].GetComponent<Image>().color = color;
-        description_Text.text = tabDescription[selectedTab];
-        StartCoroutine(SelectedTabEffectCoroutine());
-    }//탭 선택
-    IEnumerator SelectedTabEffectCoroutine()
-    {
-        while(currentActivated == Activated.Tab)
-        {
-            Color color = selectedTabImages[selectedTab].GetComponent<Image>().color;
-            while(color.a < 0.5f)
-            {
-                color.a += 0.03f;
-                selectedTabImages[selectedTab].GetComponent<Image>().color = color;
-                yield return waitTime;
-            }
-            while (color.a > 0f)
-            {
-                color.a -= 0.03f;
-                selectedTabImages[selectedTab].GetComponent<Image>().color = color;
-                yield return waitTime;
-            }
-            yield return waitTime;
-        }
-
-    }//선택된 탭 점멸효과
-
-    public void ShowItem()
-    {
         inventoryTabList.Clear();
         RemoveSlot();
-        selectedItem = 0;
-        page = 0;
+        //selectedItem = 0;
+        //page = 0;
 
-        switch(selectedTab)
+        switch (selectedTab)
         {
             case 0:
-                for(int i = 0; i < inventoryItemList.Count; i++)
+                for (int i = 0; i < inventoryItemList.Count; i++)
                 {
                     if (inventoryItemList[i].type == Item.ItemType.clue)
                         inventoryTabList.Add(inventoryItemList[i]);
@@ -155,13 +125,13 @@ public class Inventory : MonoBehaviour, Manager
                     if (inventoryItemList[i].type == Item.ItemType.person)
                         inventoryTabList.Add(inventoryItemList[i]);
                 }
-                break;           
+                break;
         } //탭에 따라 분류, 인벤토리 리스트에 추가
 
         ShowPage();
 
         SelectedItem();
-    }//슬롯 활성화
+    }//탭 활성화
     public void ShowPage()
     {
         slotCount = -1;
@@ -183,14 +153,14 @@ public class Inventory : MonoBehaviour, Manager
         {
             Color color = slots[0].selected_Item.GetComponent<Image>().color;
             color.a = 0f;
-            for (int i = 0; i <= slotCount; i++)           
+            for (int i = 0; i <= slotCount; i++)
                 slots[i].selected_Item.GetComponent<Image>().color = color;
-            description_Text.text = inventoryTabList[selectedItem].itemDescription;
+            name_text.text = inventoryTabList[selectedItem].itemName;
             StartCoroutine(SelectedItemEffectCoroutine());
         }
         else
-            description_Text.text = "아이템이 없습니다.";
-    }//슬롯 선택
+            name_text.text = "아이템이 없습니다.";
+    }//슬롯 선택 표시
     IEnumerator SelectedItemEffectCoroutine()
     {
         while (currentActivated == Activated.Item)
@@ -213,6 +183,13 @@ public class Inventory : MonoBehaviour, Manager
 
     }//선택된 슬롯 점멸효과
     
+    public void ShowItem()
+    {
+        name_text.text = inventoryTabList[selectedItem].itemName;
+        description_Text.text = inventoryTabList[selectedItem].itemDescription;
+    }//슬롯 활성화
+    
+    
     public void HandleInput()
     {
         if (!stopKeyInput)
@@ -220,34 +197,22 @@ public class Inventory : MonoBehaviour, Manager
             switch (currentActivated)
             {
                 case Activated.Tab:
-                    if (Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.X) && !preventExc)
+                    if (Input.GetKeyDown(KeyCode.X) && !preventExc)
                     {
-                        ui.ExitInventory();////////////////////////////////////////////////////////////
+                        ui.ExitInventory();
                     }
                     else if (Input.GetKeyDown(KeyCode.RightArrow))
                     {
-                        if (selectedTab < selectedTabImages.Length - 1)
-                            selectedTab++;
-                        else
-                            selectedTab = 0;
-                        ui.PlaySound(key_sound);
-                        SelectedTab();
+                        
                     }
                     else if (Input.GetKeyDown(KeyCode.LeftArrow))
                     {
-                        if (selectedTab > 0)
-                            selectedTab--;
-                        else
-                            selectedTab = selectedTabImages.Length - 1;
-                        ui.PlaySound(key_sound);
-                        SelectedTab();
+                        
                     }
-                    else if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.DownArrow))
+                    else if (Input.GetKeyDown(KeyCode.Z))
                     {
                         ui.PlaySound(enter_sound);
-                        Color color = selectedTabImages[selectedTab].GetComponent<Image>().color;
-                        color.a = 0.6f;
-                        selectedTabImages[selectedTab].GetComponent<Image>().color = color;
+                        
                         currentActivated = Activated.Item;
                         preventExc = true;
 
@@ -382,7 +347,7 @@ public class Inventory : MonoBehaviour, Manager
         ui = _ui;
 
         ui.PlaySound(open_sound);
-        go.SetActive(true);
+        //go.SetActive(true);
         selectedTab = 0;
         currentActivated = Activated.Tab;
         ShowTab();
