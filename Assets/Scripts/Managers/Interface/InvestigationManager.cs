103109a3a7735b956a586c6f72be250e36821040
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2020-02-16 16:26:03 +0900

    making cursor

--- Assets/Scripts/Managers/Interface/InvestigationManager.cs
+++ Assets/Scripts/Managers/Interface/InvestigationManager.cs
@@ -9,7 +9,8 @@ public class InvestigationManager : MonoBehaviour, Manager
 
     public void Enter(UI ui)
     {
-        throw new System.NotImplementedException();
+        SetCursor();
+        //이벤트를 비울 것.
     }
 
     public void Exit(bool _b = true)
@@ -20,20 +21,40 @@ public class InvestigationManager : MonoBehaviour, Manager
     public void HandleInput()
     {
         HandleMovement();
+
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            //이벤트가 있는가?
+        }
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            //나가기
+        }
     }
     public void HandleMovement()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector3 move = new Vector3(horizontal, vertical, 0);
+
         Vector3 position = cursor.transform.position;
         position += move * 3 * Time.deltaTime;
-        Vector3 viewPos = Camera.main.ViewportToWorldPoint(cursor.transform.position);
+
+        Vector3 viewPos = Camera.main.ViewportToWorldPoint(position);
         {
             if (viewPos.x < 0f) viewPos.x = 0f;
             if (viewPos.x > 1f) viewPos.x = 1f;
             if (viewPos.y < 0f) viewPos.y = 0f;
             if (viewPos.y > 1f) viewPos.y = 1f;
+        }
+        cursor.transform.position = Camera.main.WorldToViewportPoint(viewPos);
+    }
+    void SetCursor(float x = 0.5f, float y = 0.5f)
+    {
+        Vector3 viewPos = Camera.main.ViewportToWorldPoint(cursor.transform.position);
+        {
+            viewPos.x = x;
+            viewPos.y = y;
         }
         cursor.transform.position = Camera.main.WorldToViewportPoint(viewPos);
     }
